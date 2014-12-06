ec2dd8396790e06604621655af2fa4fc44f285cf
Antti Minkkinen <g7786@student.jamk.fi>
2014-12-06 21:25:01 +0200

    Added some data checking for addPrice.

--- src/main/java/fi/jamk/productlister/controller/PriceController.java
+++ src/main/java/fi/jamk/productlister/controller/PriceController.java
@@ -18,55 +18,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/data")
 public class PriceController {
+
 	private PriceService priceService = new PriceService();
-	
+
 	/**
-	 * Get a list of prices based on parameters. At least one parameter must be > 0.
+	 * Get a list of prices based on parameters. At least one parameter must be
+	 * > 0.
+	 *
 	 * @param productIdString
 	 * @param shopIdString
 	 * @return list of prices
 	 */
 	@RequestMapping("/prices")
 	public HashMap<String, Object> getPrices(@RequestParam(value = "productId", required = false, defaultValue = "0") String productIdString,
-											 @RequestParam(value = "shopId", required = false, defaultValue = "0") String shopIdString){
+			@RequestParam(value = "shopId", required = false, defaultValue = "0") String shopIdString) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
-		
-		System.out.println("Got a request to list prices, productId=" + productIdString+", shopId="+shopIdString);
-		try{
+
+		System.out.println("Got a request to list prices, productId=" + productIdString + ", shopId=" + shopIdString);
+		try {
 			int productId = Integer.parseInt(productIdString);
 			int shopId = Integer.parseInt(shopIdString);
-			
+
 			List<Price> priceList = priceService.getPrices(productId, shopId);
-			
+
 			result.put("success", "1");
 			result.put("prices", priceList);
-		} catch (Exception e){
+		} catch (Exception e) {
 			result.put("success", "0");
 			result.put("message", e.getMessage());
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Accepts a price as JSON and attempts to save it to database.
+	 *
 	 * @param p Price
 	 * @return JSON saved priceId
 	 */
 	@RequestMapping("/addprice")
-	public HashMap<String, Object> addPrice(@RequestBody Price p){
+	public HashMap<String, Object> addPrice(@RequestBody Price p) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		System.out.println("Got a request to add price: " + p);
 		try {
-			if(p.isValid()){
-				priceService.addPrice(p);
-			}
+			p.isValid();
 		} catch (Exception ex) {
 			result.put("success", "0");
 			result.put("message", ex.getMessage());
-			return result;
 		}
-		result.put("success", "1");
-		result.put("priceId", p.getIdPrice());
+		try {
+			priceService.addPrice(p);
+			result.put("success", "1");
+			result.put("priceId", p.getIdPrice());
+		} catch (Exception e) {
+			result.put("success", "0");
+			result.put("message", e.getMessage());
+		}
 		return result;
 	}
 }
--- src/main/java/fi/jamk/productlister/service/impl/PriceService.java
+++ src/main/java/fi/jamk/productlister/service/impl/PriceService.java
@@ -4,6 +4,8 @@ package fi.jamk.productlister.service.impl;
 
 import fi.jamk.productlister.EMHelper;
 import fi.jamk.productlister.model.Price;
+import fi.jamk.productlister.model.Product;
+import fi.jamk.productlister.model.Shop;
 import fi.jamk.productlister.service.IPriceService;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -13,68 +15,92 @@ import javax.persistence.EntityManager;
  * @author Antti Minkkinen
  */
 public class PriceService implements IPriceService {
-	
+
 	/**
 	 * Add a default price for testing.
 	 */
-	public PriceService(){
+	public PriceService() {
 		EntityManager em = EMHelper.getEM();
 		em.getTransaction().begin();
-		
+
 		Price p1 = new Price();
 		p1.setProductId(1);
 		p1.setShopId(1);
 		p1.setQuantityPrice(0.98);
-		
+
 		em.persist(p1);
 		em.getTransaction().commit();
 		em.close();
 	}
-	
+
 	/**
 	 * Adds a price to database.
+	 *
 	 * @param p
 	 * @throws Exception if prices are not correct.
 	 */
 	@Override
 	public void addPrice(Price p) throws Exception {
-		
-		if(p.isValid()){
-			EntityManager em = EMHelper.getEM();
-			em.getTransaction().begin();
-			em.persist(p);
-			em.getTransaction().commit();
-			em.close();
+
+		if (!p.isValid()) {
+			throw new Exception("Price is invalid.");
 		}
+
+		EntityManager em = EMHelper.getEM();
+		em.getTransaction().begin();
+
+		// Check if the Shop and Product exist.
+		if (em.find(Product.class, p.getProductId()) == null) {
+			em.getTransaction().rollback();
+			em.close();
+			throw new Exception("Product of the price does not exist.");
+		} else {
+
+		}
+		if (em.find(Shop.class, p.getShopId()) == null) {
+			em.getTransaction().rollback();
+			em.close();
+			throw new Exception("Shop of the price does not exist.");
+		}
+		try {
+			em.persist(p);
+		} catch(Exception e){
+			throw new Exception("That price for that product in that shop already exists.");
+		}
+		em.getTransaction().commit();
+		em.close();
+
 	}
-	
+
 	/**
-	 * Gets a list of prices based on parameters. At least one of these needs to be > 0.
+	 * Gets a list of prices based on parameters. At least one of these needs to
+	 * be > 0.
+	 *
 	 * @param productId
 	 * @param shopId
-	 * @return 
+	 * @return
 	 */
 	@Override
 	public List<Price> getPrices(int productId, int shopId) throws Exception {
 		EntityManager em = EMHelper.getEM();
 		String query = "";
-		if(productId > 0 && shopId > 0){
-			query = " WHERE p.productId = "+productId+" AND p.shopId = "+shopId;
-		} else if(productId > 0 && shopId < 1){
-			query = " WHERE p.productId = "+productId;
-		} else if(productId < 1 && shopId > 0){
-			query = " WHERE p.shopId = "+shopId;
-		} else if(productId < 1 && shopId < 1){
+		if (productId > 0 && shopId > 0) {
+			query = " WHERE p.productId = " + productId + " AND p.shopId = " + shopId;
+		} else if (productId > 0 && shopId < 1) {
+			query = " WHERE p.productId = " + productId;
+		} else if (productId < 1 && shopId > 0) {
+			query = " WHERE p.shopId = " + shopId;
+		} else if (productId < 1 && shopId < 1) {
 			// dont allow searches where neither parameter is defined properly
 			throw new Exception("productId or shopId needs to be > 0");
 		}
 		em.getTransaction().begin();
 
-		List<Price> result = em.createQuery("from Price p"+query, Price.class).getResultList();
+		List<Price> result = em.createQuery("from Price p" + query, Price.class).getResultList();
 		em.getTransaction().commit();
-		
+
 		em.close();
 		return result;
 	}
-	
+
 }
